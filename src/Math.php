846cbb2ab0cee2af417c945d14eda4e3f60029fc
DWWM <rodolphe.vincentmarques@gmail.com>
2025-10-28 14:51:04 +0100

    � test : erreur volontaire pour vérif du pipeline

--- src/Math.php
+++ src/Math.php
@@ -6,7 +6,7 @@ class Math
 {
     public function add(float $a, float $b): float
     {
-        return $a + $b;
+        return $a + $b
     }
 
     public function subtract(float $a, float $b): float
